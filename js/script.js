abbf13a15e94669c46aeed52333a9629a89254fd
Diego1548245 <148561259+Diego1548245@users.noreply.github.com>
2023-12-01 12:04:55 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const registerLink = document.querySelector('.register-link');
 const loginLink = document.querySelector('.login-link');
 const forgotLink = document.querySelector('.forgot-password');
 const loginForgotLink = document.querySelector('.login-forgot-link');
+const changePass = document.querySelector('.change-password');
+const changePassLogin = document.querySelector('.changePass-login');
 
 registerLink.onclick = () => {
     wrapper.classList.add('register');
@@ -18,4 +20,12 @@ forgotLink.onclick = () => {
 
 loginForgotLink.onclick = () => {
     wrapper.classList.remove('forgot');
+}
+
+changePass.onclick = () => {
+    wrapper.classList.add('change');
+}
+
+changePassLogin.onclick = () => {
+    wrapper.classList.remove('change');
 }
